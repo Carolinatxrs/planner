c559df78978bd9456a9b47defe825518f60e3f76
Carolina Teixeira <maria.silva@creathus.org.br>
2024-07-11 00:09:05 -0400

    feat: create participant confirmation endpoint

---
+++ src/main/java/com/rocketseat/planner/participant/ParticipantRepository.java
@@ -0,0 +1,7 @@
+package com.rocketseat.planner.participant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ParticipantRepository extends JpaRepository<Participant, UUID> { }
--- src/main/java/com/rocketseat/planner/participant/ParticipantService.java
+++ src/main/java/com/rocketseat/planner/participant/ParticipantService.java
@@ -1,5 +1,7 @@
 package com.rocketseat.planner.participant;
 
+import com.rocketseat.planner.trip.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,7 +10,14 @@ import java.util.UUID;
 @Service
 public class ParticipantService {
 
-    public void registerParticipantsToTrip(List<String> participantsToInvite, UUID tripId) {}
+    @Autowired
+    private ParticipantRepository repository;
+    public void registerParticipantsToTrip(List<String> participantsToInvite, Trip trip) {
+        List<Participant> participants = participantsToInvite.stream().map(email -> new Participant(email, trip)).toList();
+
+        this.repository.saveAll(participants);
+        System.out.println(participants.get(0).getId());
+    }
 
     public void triggerConfirmationEmailToParticipants(UUID tripId) {};
 }
