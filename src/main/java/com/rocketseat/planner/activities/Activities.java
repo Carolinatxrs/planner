7544952c57907420bead7d490ac1102e80b7b52c
Carolina Teixeira <maria.silva@creathus.org.br>
2024-07-18 00:34:49 -0400

    feat: create Activities entity

---
+++ src/main/java/com/rocketseat/planner/activities/Activities.java
@@ -0,0 +1,33 @@
+package com.rocketseat.planner.activities;
+
+import com.rocketseat.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "activities")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Activities {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "occurs_at", nullable = false)
+    private LocalDateTime occursAt;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
